8723fe6be9063d03ba44f57541b0cb996b05a472
adorgan <adorgan@gmail.com>
2022-01-05 17:41:36 -0600

    added glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,118 @@
 #include <chrono>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main(int argc, char** argv)
 {
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
-		std::exit(EXIT_FAILURE);
-	}
+	// if (argc != 4)
+	// {
+	// 	std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+	// 	std::exit(EXIT_FAILURE);
+	// }
 
-	int videoScale = std::stoi(argv[1]);
-	int cycleDelay = std::stoi(argv[2]);
-	char const* romFilename = argv[3];
+	// int videoScale = std::stoi(argv[1]);
+	// int cycleDelay = std::stoi(argv[2]);
+	// char const* romFilename = argv[3];
+	int videoScale = 20;
 
-	Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale, VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
+	// glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	Chip8 chip8;
-	chip8.loadROM(romFilename);
 
-	int videoPitch = sizeof(chip8.screen[0]) * VIDEO_WIDTH;
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(64 * videoScale, 32 * videoScale, "Chip-8 With Shaders", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	auto lastCycleTime = std::chrono::high_resolution_clock::now();
-	bool quit = false;
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
 
-	while (!quit)
-	{
-		quit = platform.ProcessInput(chip8.key);
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
 
-		auto currentTime = std::chrono::high_resolution_clock::now();
-		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+        // render
+        // ------
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		if (dt > cycleDelay)
-		{
-			lastCycleTime = currentTime;
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-			chip8.cycle();
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
 
-			platform.Update(chip8.screen, videoPitch);
-		}
-	}
+	// Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale, VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
 
-	return 0;
+	// Chip8 chip8;
+	// chip8.loadROM(romFilename);
+
+	// int videoPitch = sizeof(chip8.screen[0]) * VIDEO_WIDTH;
+
+	// auto lastCycleTime = std::chrono::high_resolution_clock::now();
+	// bool quit = false;
+
+	// while (!quit)
+	// {
+	// 	quit = platform.ProcessInput(chip8.key);
+
+	// 	auto currentTime = std::chrono::high_resolution_clock::now();
+	// 	float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+
+	// 	if (dt > cycleDelay)
+	// 	{
+	// 		lastCycleTime = currentTime;
+
+	// 		chip8.cycle();
+
+	// 		platform.Update(chip8.screen, videoPitch);
+	// 	}
+	// }
+
+	// return 0;
+}
+
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
